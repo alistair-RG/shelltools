3b4bf3e563f34f9b24236d7ed25104306ae9b6be
alistair-RG <101398443+alistair-RG@users.noreply.github.com>
2022-10-14 14:41:03 +0100

    Updated to make report selection better
    It now picks the last of the drift reports rather than the last of any report

--- drift_detector/drift_detector_tests.sh
+++ drift_detector/drift_detector_tests.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 ####### This is our single line of magic that we want to test in dfferent scenarios
+
 command()
 {
     echo "Parsing:" $1
     # --exit-status - return code is based on result of query
     # .individualResults[-1] - pick the last object in the array of results (this will be the most recent)
     # | not - Flyway returns 'true' if drift detected, we need to invert this to be able to get a non-zero return code from the --exit-status flag
-    # Limitations: If you have a report with multiple report types in it (check, code ..) then it will look for the last object in the array regardless
-    jq --exit-status '.individualResults[-1].driftDetected | not' $1
+    # Limitations - it will fail if you give it an empty JSON file/array
+    # How does it work ?
+    # [.individualResults[] - break the array of reports up
+    # select(.operation=="drift") - pick the ones that are drift reports
+    # .driftDetected - pick out just the field we are interested in
+    # There are brackets [ ] around the preceeding section to get jq to produce a formatted array of results for the next step
+    # .[-1] - take the last record in the array
+    jq --exit-status '[.individualResults[] | select(.operation=="drift") | .driftDetected ] | .[-1] | not' $1
     global_result=$?
 }
 ###### Tests
@@ -25,17 +32,24 @@ if [ $global_result -ne 1 ]; then
     exit 1
 fi
 ##############
-echo "3: Testing for drift correctly identified and flagged (based on earliest report) in a file with several drift reports"
+echo "3: Testing for drift correctly identified and flagged (based on last report) in a file with several drift reports"
 command "drift_example_2.json"
 if [ $global_result -ne 1 ]; then
     echo "*** TEST FAIL: drift should be presented but return value suggests it is not"
     exit 1
 fi
 ##############
-echo "4: Testing it doesn't do anything stupid with an empty JSON file"
-command "empty.json"
-if [ $global_result -ne 0 ]; then
-    echo "*** TEST FAIL: nothing in the file so no drift should be flagged"
+# echo "4: Testing it doesn't do anything stupid with an empty JSON file"
+# command "empty.json"
+# if [ $global_result -ne 0 ]; then
+#     echo "*** TEST FAIL: nothing in the file so no drift should be flagged"
+#     exit 1
+# fi
+##############
+echo "5: Multiple checks in a single json, make sure it finds the last drift report"
+command "drift_and_change.json"
+if [ $global_result -ne 1 ]; then
+    echo "*** TEST FAIL: drift should be presented but return value suggests it is not"
     exit 1
 fi
 
