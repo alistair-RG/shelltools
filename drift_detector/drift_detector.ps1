b4deda4eb9dc3e78d2c750394b1dede6fa022d88
alistair-RG <101398443+alistair-RG@users.noreply.github.com>
2022-10-14 18:07:04 +0100

    Update drift_detector.ps1
    Added some documentation

--- drift_detector/drift_detector.ps1
+++ drift_detector/drift_detector.ps1
@@ -1,3 +1,11 @@
+<# 
+Usage:
+   script_name.ps1 json_text_file
+Description:
+    Reads in the Flyway check report (json) and parses it to find the last drift report contained in there.
+    If the driftDetected field is True then this will return an exit code of 1, otherwise 0
+    This can be used to halt a pipeline
+#>
 $filename =$args[0]
 $ip = Get-Content $filename -Raw | convertfrom-json
 # Extract all the objects that are drift reports
